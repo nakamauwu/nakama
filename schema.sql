1deab3ac391bc98a7c1caa27edf45b32d649f434
Nicolás Parada <parada.nicolas@outlook.com>
2018-12-02 18:34:39 -0300

    TODO: add follow foreign keys

--- schema.sql
+++ schema.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
     followees_count INT NOT NULL DEFAULT 0 CHECK (followees_count >= 0)
 );
 
+-- TODO: add follow foreign keys.
 CREATE TABLE IF NOT EXISTS follows (
     follower_id INT NOT NULL,
     followee_id INT NOT NULL,
