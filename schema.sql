d3eb97981aaa304ef6ff12be3b7af50eb96fc3eb
Nicolás Parada <parada.nicolas@outlook.com>
2018-12-07 13:58:53 -0300

    feat: foreign keys on follow relationship

--- schema.sql
+++ schema.sql
@@ -10,10 +10,9 @@ CREATE TABLE IF NOT EXISTS users (
     followees_count INT NOT NULL DEFAULT 0 CHECK (followees_count >= 0)
 );
 
--- TODO: add follow foreign keys.
 CREATE TABLE IF NOT EXISTS follows (
-    follower_id INT NOT NULL,
-    followee_id INT NOT NULL,
+    follower_id INT NOT NULL REFERENCES users,
+    followee_id INT NOT NULL REFERENCES users,
     PRIMARY KEY (follower_id, followee_id)
 );
 
