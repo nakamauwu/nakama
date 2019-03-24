972df4aea89be2c0413f26920be1d96e4d79daea
Nicolás Parada <parada.nicolas@outlook.com>
2019-03-24 12:57:07 -0300

    change: same icon for like button

--- web/static/js/components/post.js
+++ web/static/js/components/post.js
@@ -1,7 +1,7 @@
 import { isAuthenticated } from '../auth.js';
 import { escapeHTML, linkify } from '../utils.js';
 import renderAvatarHTML from './avatar.js';
-import { heartFilledSVG, heartOutlinedSVG } from './heart-icons.js';
+import heartIconSVG from './heart-icon.js';
 
 // #region icons
 const squareMessageSVG = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="message-square"><rect width="24" height="24" opacity="0"/><circle cx="12" cy="11" r="1"/><circle cx="16" cy="11" r="1"/><circle cx="8" cy="11" r="1"/><path d="M19 3H5a3 3 0 0 0-3 3v15a1 1 0 0 0 .51.87A1 1 0 0 0 3 22a1 1 0 0 0 .51-.14L8 19.14a1 1 0 0 1 .55-.14H19a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3zm1 13a1 1 0 0 1-1 1H8.55a3 3 0 0 0-1.55.43l-3 1.8V6a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1z"/></g></g></svg>`
@@ -38,12 +38,12 @@ export default function renderPost(post, timelineItemID) {
                     aria-pressed="${post.liked}"
                     aria-label="${post.likesCount} likes">
                     <span class="likes-count">${post.likesCount}</span>
-                    ${post.liked ? heartFilledSVG : heartOutlinedSVG}
+                    ${heartIconSVG}
                 </button>
             ` : `
                 <span class="brick" aria-label="${post.likesCount} likes">
                     <span>${post.likesCount}</span>
-                    ${post.liked ? heartFilledSVG : heartOutlinedSVG}
+                    ${heartIconSVG}
                 </span>
             `}
             <a class="brick comments-link"
