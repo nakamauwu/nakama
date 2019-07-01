8b60e07fca369e8aad45c7c146d9fc7ce94a3d86
Nicolás Parada <parada.nicolas@outlook.com>
2019-06-30 21:28:00 -0400

    restyle wip

--- web/static/js/components/home-page.js
+++ web/static/js/components/home-page.js
@@ -12,7 +12,7 @@ template.innerHTML = `
     <div class="container">
         <h1>Timeline</h1>
         <form id="post-form" class="post-form">
-            <textarea placeholder="Write something..." maxlength="480" required></textarea>
+            <textarea name="content" placeholder="Write something..." maxlength="480" required></textarea>
             <button class="post-form-button" hidden>
                 <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="paper-plane"><rect width="24" height="24" opacity="0"/><path d="M21 4a1.31 1.31 0 0 0-.06-.27v-.09a1 1 0 0 0-.2-.3 1 1 0 0 0-.29-.19h-.09a.86.86 0 0 0-.31-.15H20a1 1 0 0 0-.3 0l-18 6a1 1 0 0 0 0 1.9l8.53 2.84 2.84 8.53a1 1 0 0 0 1.9 0l6-18A1 1 0 0 0 21 4zm-4.7 2.29l-5.57 5.57L5.16 10zM14 18.84l-1.86-5.57 5.57-5.57z"/></g></g></svg>
                 <span>Publish</span>
