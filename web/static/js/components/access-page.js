43f229da3769f3e75a4d19c8e4d3ecf876a530c7
Nicolás Parada <parada.nicolas@outlook.com>
2021-03-15 23:42:47 -0300

    web: default access email only on localhost

--- web/static/js/components/access-page.js
+++ web/static/js/components/access-page.js
@@ -10,7 +10,7 @@ template.innerHTML = `
         <p>Welcome to Nakama, the next social network for anime fans 🤗</p>
         <h2>Access</h2>
         <form id="login-form" class="login-form">
-            <input type="email" name="email" placeholder="Email" autocomplete="email" value="shinji@example.org" required>
+            <input type="email" name="email" placeholder="Email" autocomplete="email" required>
             <button>
                 <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="log-in"><rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0"/><path d="M19 4h-2a1 1 0 0 0 0 2h1v12h-1a1 1 0 0 0 0 2h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1z"/><path d="M11.8 7.4a1 1 0 0 0-1.6 1.2L12 11H4a1 1 0 0 0 0 2h8.09l-1.72 2.44a1 1 0 0 0 .24 1.4 1 1 0 0 0 .58.18 1 1 0 0 0 .81-.42l2.82-4a1 1 0 0 0 0-1.18z"/></g></g></svg>
                 <span>Login</span>
@@ -25,7 +25,11 @@ template.innerHTML = `
 export default function renderAccessPage() {
     const page = /** @type {DocumentFragment} */ (template.content.cloneNode(true))
     const loginForm = /** @type {HTMLFormElement} */ (page.getElementById("login-form"))
+    const emailInput = loginForm.querySelector("input")
     loginForm.addEventListener("submit", onLoginFormSubmit)
+    if (isLocalhost()) {
+        emailInput.value = "shinji@example.org"
+    }
     return page
 }
 
