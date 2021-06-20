23570500580ab93b0d2be9ae24d1d5a7f8f34428
Nicolás Parada <parada.nicolas@outlook.com>
2021-06-19 21:46:48 -0400

    whoops: do not delete db

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 -- CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-DROP DATABASE IF EXISTS nakama CASCADE;
+-- DROP DATABASE IF EXISTS nakama CASCADE;
 CREATE DATABASE IF NOT EXISTS nakama;
 SET DATABASE = nakama;
 
@@ -116,16 +116,16 @@ CREATE TABLE IF NOT EXISTS notifications (
     UNIQUE INDEX unique_notifications (user_id, type, post_id, read_at)
 );
 
-INSERT INTO users (id, email, username) VALUES
-    ('24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'shinji@example.org', 'shinji'),
-    ('93dfcef9-0b45-46ae-933c-ea52fbf80edb', 'rei@example.org', 'rei');
+-- INSERT INTO users (id, email, username) VALUES
+--     ('24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'shinji@example.org', 'shinji'),
+--     ('93dfcef9-0b45-46ae-933c-ea52fbf80edb', 'rei@example.org', 'rei');
 
-INSERT INTO posts (id, user_id, content, comments_count) VALUES
-    ('c592451b-fdd2-430d-8d49-e75f058c3dce', '24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'sample post', 1);
-INSERT INTO post_subscriptions (user_id, post_id) VALUES
-    ('24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'c592451b-fdd2-430d-8d49-e75f058c3dce');
-INSERT INTO timeline (id, user_id, post_id) VALUES
-    ('d7490258-1f2f-4a75-8fbb-1846ccde9543', '24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'c592451b-fdd2-430d-8d49-e75f058c3dce');
+-- INSERT INTO posts (id, user_id, content, comments_count) VALUES
+--     ('c592451b-fdd2-430d-8d49-e75f058c3dce', '24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'sample post', 1);
+-- INSERT INTO post_subscriptions (user_id, post_id) VALUES
+--     ('24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'c592451b-fdd2-430d-8d49-e75f058c3dce');
+-- INSERT INTO timeline (id, user_id, post_id) VALUES
+--     ('d7490258-1f2f-4a75-8fbb-1846ccde9543', '24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'c592451b-fdd2-430d-8d49-e75f058c3dce');
 
-INSERT INTO comments (id, user_id, post_id, content) VALUES
-    ('648e60bf-b0ab-42e6-8e48-10f797b19c49', '24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'c592451b-fdd2-430d-8d49-e75f058c3dce', 'sample comment');
+-- INSERT INTO comments (id, user_id, post_id, content) VALUES
+--     ('648e60bf-b0ab-42e6-8e48-10f797b19c49', '24ca6ce6-b3e9-4276-a99a-45c77115cc9f', 'c592451b-fdd2-430d-8d49-e75f058c3dce', 'sample comment');
