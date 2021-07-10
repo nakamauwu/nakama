40517cbcc35bf95d5c2c7a02ee8545a5874c051d
Nicolás Parada <parada.nicolas@outlook.com>
2021-07-09 22:24:34 -0400

    transport/http: fix test build

--- transport/http/auth_test.go
+++ transport/http/auth_test.go
@@ -116,7 +116,7 @@ func Test_handler_sendMagicLink(t *testing.T) {
 	for _, tc := range tt {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			h := New(tc.svc, nil, log.NewNopLogger(), nil, nil, true)
+			h := New(tc.svc, nil, nil, log.NewNopLogger(), nil, nil, true)
 			srv := httptest.NewServer(h)
 			defer srv.Close()
 
