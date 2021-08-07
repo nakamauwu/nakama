fc1f9e89240544c09721841cfd54060df3bc7b5a
nicolasparada <parada.nicolas@outlook.com>
2021-08-06 23:29:19 -0400

    web/app: trust puush.me

--- web/app/components/post-item.js
+++ web/app/components/post-item.js
@@ -375,7 +375,7 @@ function collectMediaURLs(s) {
     return out
 }
 
-const trustedOrigins = ["https://i.imgur.com"]
+const trustedOrigins = ["https://i.imgur.com", "https://puu.sh"]
 const imageExts = ["jpg", "jpeg", "gif", "png", "webp", "avif"].map(ext => "." + ext)
 const audioExts = ["wav", "mp3", "flac"].map(ext => "." + ext)
 const videoExts = ["mp4", "webm", "mov", "3gp", "ogg"].map(ext => "." + ext)
