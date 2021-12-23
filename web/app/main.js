8d5aa79aa5ab30d405354408eb60399a345a20e1
Nicolás Parada <parada.nicolas@outlook.com>
2021-12-23 01:00:28 -0300

    christmas time

--- web/app/main.js
+++ web/app/main.js
@@ -250,3 +250,42 @@ const isHalloween = now.getMonth() === 9 && now.getDate() === 31
 if (isHalloween) {
     document.documentElement.classList.add("halloween")
 }
+
+const isChristmas = now.getMonth() === 11
+if (isChristmas) {
+    import("canvas-confetti").then(function makeItSnow({ default: confetti }) {
+        const duration = 5 * 60 * 1000 // 5 minutes
+        const animationEnd = Date.now() + duration
+        let skew = 1
+
+        function randomInRange(min, max) {
+            return Math.random() * (max - min) + min
+        }
+
+        (function frame() {
+            const timeLeft = animationEnd - Date.now()
+            const ticks = Math.max(200, 500 * (timeLeft / duration))
+            skew = Math.max(0.8, skew - 0.001)
+
+            confetti({
+                particleCount: 1,
+                startVelocity: 0,
+                ticks: ticks,
+                origin: {
+                    x: Math.random(),
+                    // since particles fall down, skew start toward the top
+                    y: (Math.random() * skew) - 0.2
+                },
+                colors: ['#ffffff'],
+                shapes: ['circle'],
+                gravity: randomInRange(0.4, 0.6),
+                scalar: randomInRange(0.4, 1),
+                drift: randomInRange(-0.4, 0.4)
+            })
+
+            if (timeLeft > 0) {
+                requestAnimationFrame(frame)
+            }
+        }())
+    })
+}
