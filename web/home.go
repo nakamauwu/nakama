2f63f1af428fc29539ff383c7f4ad60e1174e64a
Nicolás Parada <parada.nicolas@outlook.com>
2022-09-03 01:05:27 -0400

    show either all posts or timeline in home page whether the user is authenticated

--- web/home.go
+++ web/home.go
@@ -13,8 +13,7 @@ type homeData struct {
 	Session
 	CreatePostErr  error
 	CreatePostForm url.Values
-	Timeline       []nakama.HomeTimelineRow
-	Posts          []nakama.PostsRow
+	Posts          any
 }
 
 func (h *Handler) renderHome(w http.ResponseWriter, data homeData, statusCode int) {
@@ -25,26 +24,25 @@ func (h *Handler) renderHome(w http.ResponseWriter, data homeData, statusCode in
 func (h *Handler) showHome(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	tt, err := h.Service.HomeTimeline(ctx)
+	var posts any
+	var err error
+
+	if _, ok := nakama.UserFromContext(ctx); ok {
+		posts, err = h.Service.HomeTimeline(ctx)
+	} else {
+		posts, err = h.Service.Posts(ctx, nakama.PostsInput{})
+	}
+
 	if err != nil {
 		h.log(err)
 		h.renderErr(w, r, err)
 		return
 	}
 
-	// TODO: show either timeline or posts based whether the user is logged in.
-	// pp, err := h.Service.Posts(ctx, nakama.PostsInput{})
-	// if err != nil {
-	// 	h.log(err)
-	// 	h.renderErr(w, r, err)
-	// 	return
-	// }
-
 	h.renderHome(w, homeData{
 		Session:        h.sessionFromReq(r),
 		CreatePostErr:  h.popErr(r, "create_post_err"),
 		CreatePostForm: h.popForm(r, "create_post_form"),
-		Timeline:       tt,
-		// Posts:          pp,
+		Posts:          posts,
 	}, http.StatusOK)
 }
