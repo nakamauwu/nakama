d6fe79c3164aee39cfbc0a03e7480e805ac2bae7
Nicolás Parada <parada.nicolas@outlook.com>
2022-12-19 17:18:11 +0000

    remove confetti

--- web/app/main.js
+++ web/app/main.js
@@ -254,39 +254,5 @@ if (isHalloween) {
 
 const isChristmas = now.getMonth() === 11
 if (isChristmas) {
-    import("canvas-confetti").then(function makeItSnow({ default: confetti }) {
-        const duration = 5 * 60 * 1000 // 5 minutes
-        const animationEnd = Date.now() + duration
-        let skew = 1
-
-        function randomInRange(min, max) {
-            return Math.random() * (max - min) + min
-        }
-
-        (function frame() {
-            const timeLeft = animationEnd - Date.now()
-            const ticks = Math.max(200, 500 * (timeLeft / duration))
-            skew = Math.max(0.8, skew - 0.001)
-
-            confetti({
-                particleCount: 1,
-                startVelocity: 0,
-                ticks: ticks,
-                origin: {
-                    x: Math.random(),
-                    // since particles fall down, skew start toward the top
-                    y: (Math.random() * skew) - 0.2
-                },
-                colors: ['#ffffff'],
-                shapes: ['circle'],
-                gravity: randomInRange(0.4, 0.6),
-                scalar: randomInRange(0.4, 1),
-                drift: randomInRange(-0.4, 0.4)
-            })
-
-            if (timeLeft > 0) {
-                requestAnimationFrame(frame)
-            }
-        }())
-    })
+    // TODO: PADORU
 }
